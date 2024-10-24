5e41982b6ba215fec439779bae2cfbac0b4ca710
Efrain Hoyos <efrainhoyos@gmail.com>
2024-10-23 23:15:05 -0500

    Se crea los procedimientos almacenados para la tabla tbl_categoria

--- sp_categoria.sql
+++ sp_categoria.sql
@@ -1,5 +1,8 @@
+
+# Procedimiento Crear una Categoria 
+
 DELIMITER $$
-CREATE PROCEDURE sp_create_categoria(
+CREATE PROCEDURE spCreateCategory(
     IN p_descripcion VARCHAR(45)
 )
 BEGIN
@@ -8,16 +11,24 @@ BEGIN
 END $$
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE sp_read_categoria()
+
+# Procedimiento mostrar categoria
+
+DELIMITER //
+CREATE PROCEDURE spGetCategory()
 BEGIN
-    SELECT cat_id, cat_descripcion 
+    SELECT 
+		cat_id AS Id,
+        cat_descripcion AS Descripcion,
     FROM tbl_categoria;
-END $$
+END //
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE sp_update_categoria(
+
+# Procedimiento actualizar categoria 
+
+DELIMITER //
+CREATE PROCEDURE spUpdateCategory(
     IN p_cat_id INT,
     IN p_descripcion VARCHAR(45)
 )
@@ -25,11 +36,15 @@ BEGIN
     UPDATE tbl_categoria 
     SET cat_descripcion = p_descripcion
     WHERE cat_id = p_cat_id;
-END $$
+END //
 DELIMITER ;
 
+
+# Delete Procedure 
+
+
 DELIMITER $$
-CREATE PROCEDURE sp_delete_categoria(
+CREATE PROCEDURE spDeleteCategory(
     IN p_cat_id INT
 )
 BEGIN
@@ -38,18 +53,16 @@ BEGIN
 END $$
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE sp_get_productos_with_categoria_and_proveedor()
+
+# Select Procedure DDL #
+
+
+DELIMITER //
+CREATE PROCEDURE spGetCategoryDDL()
 BEGIN
     SELECT 
-        p.prod_nombre AS Producto,
-        c.cat_descripcion AS Categoria,
-        pr.prov_id AS Proveedor
-    FROM 
-        tbl_producto p
-    INNER JOIN 
-        tbl_categoria c ON p.tbl_categoria_cat_id = c.cat_id
-    INNER JOIN 
-        tbl_proveedor pr ON p.tbl_proveedor_prov_id = pr.prov_id;
-END $$
+		cat_id AS Id,
+        cat_descripcion AS Descripcion,
+    FROM tbl_categoria;
+END //
 DELIMITER ;
