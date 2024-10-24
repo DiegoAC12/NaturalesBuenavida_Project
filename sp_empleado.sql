94c6ee71519c1f02028dc7b075de47bed16ca327
Efrain Hoyos <efrainhoyos@gmail.com>
2024-10-24 01:46:14 -0500

    Se crea los procedimientos almacenados de la tabla tbl_empleado

--- sp_empleado.sql
+++ sp_empleado.sql
@@ -1,23 +1,35 @@
-DELIMITER $$
-CREATE PROCEDURE sp_create_empleado(
-    IN p_persona_id INT
+# Procedimiento Insertar Empleado
+
+DELIMITER //
+CREATE PROCEDURE spInsertEmployee(
+    IN p_fkpersona_id INT
 )
 BEGIN
     INSERT INTO tbl_empleado (tbl_persona_pers_id)
-    VALUES (p_persona_id);
-END $$
+    VALUES (p_fkpersona_id);
+END //
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE sp_read_empleado()
+#Procedimiento mostrar empleado
+
+DELIMITER //
+CREATE PROCEDURE spGetEmployee()
 BEGIN
-    SELECT emp_id, tbl_persona_pers_id 
-    FROM tbl_empleado;
-END $$
+    SELECT 
+		per.pers_identificacion AS Identificacion,
+        per.pers_nombre_razonsocial AS Nombre,
+        per.pers_apellido AS Apellido,
+        per.pers_telefono AS Telefono,
+        per.pers_correo_electronico AS Correo
+    FROM tbl_empleado as emp
+    INNER JOIN tbl_persona AS per ON emp.emp_id = per.pers_id;
+END //
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE sp_update_empleado(
+# Procedimiento Actualizar Empleado
+
+DELIMITER //
+CREATE PROCEDURE spUpdateEmployee(
     IN p_emp_id INT,
     IN p_persona_id INT
 )
@@ -25,15 +37,30 @@ BEGIN
     UPDATE tbl_empleado 
     SET tbl_persona_pers_id = p_persona_id
     WHERE emp_id = p_emp_id;
-END $$
+END //
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE sp_delete_empleado(
+#Procedimiento eliminar empleado
+
+DELIMITER //
+CREATE PROCEDURE spDeleteEmployee(
     IN p_emp_id INT
 )
 BEGIN
     DELETE FROM tbl_empleado
     WHERE emp_id = p_emp_id;
-END $$
+END //
+DELIMITER ;
+
+#Procedimiento mostrar empleado DDL
+
+DELIMITER //
+CREATE PROCEDURE spGetEmployeeDDL()
+BEGIN
+    SELECT 
+        emp.emp_id AS Id,
+        CONCAT(per.pers_nombre_razonsocial, " ", per.pers_apellido) AS NombreCompleto
+    FROM tbl_empleado as emp
+    INNER JOIN tbl_persona AS per ON emp.emp_id = per.pers_id;
+END //
 DELIMITER ;
