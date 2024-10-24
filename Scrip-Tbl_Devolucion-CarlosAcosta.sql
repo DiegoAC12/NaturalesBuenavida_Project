76a3bdc437a3ba48f8895bcdfe82f51138b6e3a8
carlos acosta <carlosangucho00@gmail.com>
2024-10-24 00:01:23 -0500

    Se actualiza el sp

--- Scrip-Tbl_Devolucion-CarlosAcosta.sql
+++ Scrip-Tbl_Devolucion-CarlosAcosta.sql
@@ -15,8 +15,31 @@ DELIMITER $$
 
 CREATE PROCEDURE sp_mostrar_devoluciones()
 BEGIN
-    SELECT * FROM tbl_devolucion;
-END $$
+    SELECT 
+        d.dev_fecha_devolucion, 
+        d.dev_motivo, 
+        v.vent_id, 
+        v.vent_fecha, 
+        v.vent_total, 
+        v.vent_descripcion,
+        p_e.pers_identificacion AS identificacion_empleado,
+        p_e.pers_nombre_razonsocial AS nombre_empleado, 
+        p_c.pers_identificacion AS identificacion_cliente,
+        p_c.pers_nombre_razonsocial AS nombre_cliente, 
+        p_c.pers_apellido AS apellido_cliente
+    FROM 
+        tbl_devolucion d
+    LEFT JOIN 
+        tbl_venta v ON d.tbl_venta_vent_id = v.vent_id
+    LEFT JOIN 
+        tbl_empleado e ON v.tbl_empleado_emp_id = e.emp_id
+    LEFT JOIN 
+        tbl_persona p_e ON e.tbl_persona_pers_id = p_e.pers_id
+    LEFT JOIN 
+        tbl_cliente c ON v.tbl_cliente_cli_id = c.cli_id
+    LEFT JOIN 
+        tbl_persona p_c ON c.tbl_persona_pers_id = p_c.pers_id;
+END$$
 
 DELIMITER ;
 DELIMITER $$
