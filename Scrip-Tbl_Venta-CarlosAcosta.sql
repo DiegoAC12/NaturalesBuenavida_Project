ff6a0f8927ca0fd91d570a65b6526803a7efc0d5
carlos acosta <carlosangucho00@gmail.com>
2024-10-23 23:58:02 -0500

    Se actualiza el sp

--- Scrip-Tbl_Venta-CarlosAcosta.sql
+++ Scrip-Tbl_Venta-CarlosAcosta.sql
@@ -16,9 +16,25 @@ DELIMITER ;tbl_cliente
 
 DELIMITER $$
 
-CREATE PROCEDURE sp_mostrar_ventas()
+CREATE  PROCEDURE sp_mostrar_ventas()
 BEGIN
-    SELECT * FROM tbl_venta;
+    SELECT 
+        v.vent_Fecha as fecha , v.vent_total as total ,v.vent_descripcion as descripcion,  
+        p_e.pers_nombre_razonsocial AS nombre_empleado, 
+        p_e.pers_apellido AS apellido_empleado, 
+        p_c.pers_identificacion AS identificacion_cliente,
+        p_c.pers_nombre_razonsocial AS nombre_cliente, 
+        p_c.pers_apellido AS apellido_cliente
+    FROM 
+        tbl_venta v
+    LEFT JOIN 
+        tbl_empleado e ON v.tbl_empleado_emp_id = e.emp_id
+    LEFT JOIN 
+        tbl_persona p_e ON e.tbl_persona_pers_id = p_e.pers_id
+    LEFT JOIN 
+        tbl_cliente c ON v.tbl_cliente_cli_id = c.cli_id
+    LEFT JOIN 
+        tbl_persona p_c ON c.tbl_persona_pers_id = p_c.pers_id;
 END $$
 
 DELIMITER ;
