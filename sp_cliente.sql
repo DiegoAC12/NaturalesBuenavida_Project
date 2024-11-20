855e8e139bbf34a6eac13f44d2364b77a10d7cc5
Diego Alzate <diegoalzate.0207@gmail.com>
2024-11-19 23:33:10 -0500

    Subiendo correción del procedimiento almacenado de la tabla cliente

--- sp_cliente.sql
+++ sp_cliente.sql
@@ -7,8 +7,16 @@ CREATE PROCEDURE spInsertCliente(
     IN p_fkpersona_id INT
 )
 BEGIN
-    INSERT INTO tbl_cliente (tbl_persona_pers_id)
-    VALUES (p_fkpersona_id);
+    -- Verificar si la persona ya está registrada como cliente
+    IF NOT EXISTS (SELECT 1 FROM tbl_cliente WHERE tbl_persona_pers_id = p_fkpersona_id) THEN
+        -- Si no está registrada, se inserta
+        INSERT INTO tbl_cliente (tbl_persona_pers_id)
+        VALUES (p_fkpersona_id);
+    ELSE
+        -- Si ya está registrada, se puede devolver un mensaje o código de error personalizado
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'La persona ya está registrada como cliente.';
+    END IF;
 END //
 DELIMITER ;
 
@@ -20,13 +28,14 @@ DELIMITER //
 CREATE PROCEDURE spSelectCliente()
 BEGIN
     SELECT 
+		cli.cli_id AS ClienteID,
 		per.pers_identificacion AS Identificacion,
         per.pers_nombre_razonsocial AS Nombre,
         per.pers_apellido AS Apellido,
         per.pers_telefono AS Telefono,
         per.pers_correo_electronico AS Email
     FROM tbl_cliente as cli
-    INNER JOIN tbl_persona AS per ON cli.cli_id = per.pers_id;
+    INNER JOIN tbl_persona AS per ON cli.tbl_persona_pers_id = per.pers_id;
 END //
 DELIMITER ;
 
