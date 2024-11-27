8f386cbe56cdcb42f3f0713d7e782e65b5abadbd
Diego Alzate <diegoalzate.0207@gmail.com>
2024-11-27 14:34:13 -0500

    Realizando nueva modificacion del procedimiento DDL de cliente

--- sp_select_clienteDDL.sql
+++ sp_select_clienteDDL.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE spSelectClienteDDL()
 BEGIN
     SELECT 
         cli.cli_id AS Id,
-        CONCAT(per.pers_nombre_razonsocial, ' ', per.pers_apellido) AS NombreCompleto
+        CONCAT(per.pers_identificacion,' - ', per.pers_nombre_razonsocial, ' ', per.pers_apellido) AS NombreCompleto
     FROM tbl_cliente as cli
     INNER JOIN tbl_persona AS per ON cli.cli_id = per.pers_id;
 END //
