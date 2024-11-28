92e24c59f94f81a8697925ed49e09d2384534613
carlos acosta <carlosangucho00@gmail.com>
2024-11-28 12:02:26 -0500

    Se modifica el sp de la tabla empleado

--- sp_empleado.sql
+++ sp_empleado.sql
@@ -1,32 +1,47 @@
-# Procedimiento Insertar Empleado
+###########################
+#### Insert Procedure #####
+###########################
 
 DELIMITER //
 CREATE PROCEDURE spInsertEmployee(
     IN p_fkpersona_id INT
 )
 BEGIN
-    INSERT INTO tbl_empleado (tbl_persona_pers_id)
-    VALUES (p_fkpersona_id);
+    -- Verificar si la persona ya está registrada como empleado
+    IF NOT EXISTS (SELECT 1 FROM tbl_empleado WHERE tbl_persona_pers_id = p_fkpersona_id) THEN
+        -- Si no está registrada, se inserta
+        INSERT INTO tbl_empleado (tbl_persona_pers_id)
+        VALUES (p_fkpersona_id);
+    ELSE
+        -- Si ya está registrada, se puede devolver un mensaje o código de error personalizado
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'La persona ya está registrada como empleado.';
+    END IF;
 END //
 DELIMITER ;
 
-#Procedimiento mostrar empleado
+###########################
+#### Select Procedure #####
+###########################
 
 DELIMITER //
 CREATE PROCEDURE spGetEmployee()
 BEGIN
     SELECT 
-		per.pers_identificacion AS Identificacion,
+        emp.emp_id AS EmpleadoID,
+        per.pers_identificacion AS Identificacion,
         per.pers_nombre_razonsocial AS Nombre,
         per.pers_apellido AS Apellido,
         per.pers_telefono AS Telefono,
         per.pers_correo_electronico AS Correo
-    FROM tbl_empleado as emp
-    INNER JOIN tbl_persona AS per ON emp.emp_id = per.pers_id;
+    FROM tbl_empleado AS emp
+    INNER JOIN tbl_persona AS per ON emp.tbl_persona_pers_id = per.pers_id;
 END //
 DELIMITER ;
 
-# Procedimiento Actualizar Empleado
+###########################
+#### Update Procedure #####
+###########################
 
 DELIMITER //
 CREATE PROCEDURE spUpdateEmployee(
@@ -40,7 +55,9 @@ BEGIN
 END //
 DELIMITER ;
 
-#Procedimiento eliminar empleado
+###########################
+#### Delete Procedure #####
+###########################
 
 DELIMITER //
 CREATE PROCEDURE spDeleteEmployee(
@@ -52,15 +69,17 @@ BEGIN
 END //
 DELIMITER ;
 
-#Procedimiento mostrar empleado DDL
+###########################
+#### Select Procedure DDL #
+###########################
 
 DELIMITER //
 CREATE PROCEDURE spGetEmployeeDDL()
 BEGIN
     SELECT 
         emp.emp_id AS Id,
-        CONCAT(per.pers_nombre_razonsocial, " ", per.pers_apellido) AS NombreCompleto
-    FROM tbl_empleado as emp
-    INNER JOIN tbl_persona AS per ON emp.emp_id = per.pers_id;
+        CONCAT(per.pers_nombre_razonsocial, ' ', per.pers_apellido) AS NombreCompleto
+    FROM tbl_empleado AS emp
+    INNER JOIN tbl_persona AS per ON emp.tbl_persona_pers_id = per.pers_id;
 END //
 DELIMITER ;
