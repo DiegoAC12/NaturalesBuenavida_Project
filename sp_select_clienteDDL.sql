9272549304af6707b53f96c75ebaf4c189ac80d0
Diego Alzate <diegoalzate.0207@gmail.com>
2024-11-29 20:23:41 -0500

    Subiendo cambios para el procedimiento almacenado DDL de la tabla Cliente

--- sp_select_clienteDDL.sql
+++ sp_select_clienteDDL.sql
@@ -5,6 +5,6 @@ BEGIN
         cli.cli_id AS Id,
         CONCAT(per.pers_identificacion,' - ', per.pers_nombre_razonsocial, ' ', per.pers_apellido) AS NombreCompleto
     FROM tbl_cliente as cli
-    INNER JOIN tbl_persona AS per ON cli.cli_id = per.pers_id;
+    INNER JOIN tbl_persona AS per ON cli.tbl_persona_pers_id = per.pers_id;
 END //
 DELIMITER ;
